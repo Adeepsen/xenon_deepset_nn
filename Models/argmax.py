26713e679a2a2e7434c4a5b1e3186a6c69697add
Adeepsen <adeeps@g.clemson.edu>
2026-06-03 13:03:10 -0400

    update argmax to remove bad data

--- Models/argmax.py
+++ Models/argmax.py
@@ -1,33 +1,45 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
 
 data = np.load("/Users/adeepsen/xenon_deepset_nn/data/s2_tag_training_clusters.npy")
 
-# Get event boundaries once
-event_ids, start_idx, counts = np.unique(
-    data["event_number"],
-    return_index=True,
-    return_counts=True
+#Removing top 13 cm of TPC data: Using 0.675 mm/us drift velocity ~192.6 us
+top13_ns = 192_600
+
+# Keep only events with 0 clusters in the top 13 cm
+event_has_top13 = (
+    pd.DataFrame(data)[["event_number", "drift_time_mean"]]
+    .groupby("event_number")["drift_time_mean"]
+    .min()
+    .lt(top13_ns)
 )
 
-def get_event_by_slice(start, count):
-    return data[start:start + count]
+bad_event_ids = event_has_top13[event_has_top13].index.to_numpy()
+good_mask = ~np.isin(data["event_number"], bad_event_ids)
+data_fid = data[good_mask]
 
-def rank_clusters_by_electrons(event):
-    return np.argsort(event["n_electrons_interface"])[::-1]
+print("Events removed:", len(bad_event_ids))
+print("Remaining p_alt > 1:", np.sum(data_fid["p_alt"] > 1))
 
+event_ids = np.unique(data_fid["event_number"])
+
+#Random sample of events to save my laptop from exploding
+sample_size = 10000
+
+if len(event_ids) > sample_size:
+    event_ids = np.random.choice(
+        event_ids,
+        size=sample_size,
+        replace=False
+    )
 main_correct = 0
 alt_correct = 0
 num_events_with_alt = 0
 
-sample_idx = np.random.choice(len(event_ids), size=10000, replace=False)
+for event_id in event_ids:
+    event = data_fid[data_fid["event_number"] == event_id]
 
-for i in sample_idx:
-    start = start_idx[i]
-    count = counts[i]
-    event = get_event_by_slice(start, count)
-
-    order = rank_clusters_by_electrons(event)
+    order = np.argsort(event["n_electrons_interface"])[::-1]
 
     pred_main_idx = order[0]
     pred_alt_idx = order[1] if len(order) > 1 else None
@@ -43,7 +55,6 @@ for i in sample_idx:
         if pred_alt_idx == true_alt_idx:
             alt_correct += 1
 
-print("Event-level argmax baseline")
-print("---------------------------")
-print("Main accuracy:", main_correct / len(sample_idx))
+print("Event-level argmax baseline on fiducial data")
+print("Main accuracy:", main_correct / len(event_ids))
 print("Alt accuracy:", alt_correct / num_events_with_alt)
